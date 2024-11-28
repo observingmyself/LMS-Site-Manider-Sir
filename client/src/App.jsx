4a95c7ea77f8b0619fd50a5e61db451968ef4300
observingmyself <ps5380028@gmail.com>
2024-11-28 15:29:54 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='text-center'>hello</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
