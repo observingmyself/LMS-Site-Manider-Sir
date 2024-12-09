33c2a26904472d67c258c6baa0ae463958a74888
observingmyself <ps5380028@gmail.com>
2024-12-09 12:18:57 +0530

    registration

---
+++ client/src/components/user/RegistrationForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const RegistrationForm = () => {
+  return (
+    <div className="mt-28">
+      <div className="bg-[#EEF4FC] text-center py-8 font-semibold">
+        <h4 className=" text-4xl md:text-5xl">Register yourself</h4>
+        <p className="mt-4 text-xl md:text-2xl">{"{WINTER CODING CAMP}"}</p>
+      </div>
+    </div>
+  );
+}
+
+export default RegistrationForm
