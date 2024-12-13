4e0c13d9b4e1545764b3501e12bb5e90ae780d69
Moksh <moksh8516@gmail.com>
2024-12-13 11:20:22 +0530

    add course controller and add destroy function on utils

--- server/src/controllers/course.controllers.js
+++ server/src/controllers/course.controllers.js
@@ -31,6 +31,69 @@ const registerCourse = asyncHandler(async (req, res) => {
     .json(new ApiResponse(201, createCourse, "Course created Successfully"))
 })
 
+const getCourse = asyncHandler(async (req, res) => {
+  const page = req.query.page || 1;
+  const limit = req.query.limit || 12;
+  const sortBy = req.query.sortBy || "cratedAt";
+  const order = req.query.order === "asc" ? 1 : -1;
+  const skip = (page - 1) * limit;
+  const data = await Course.find().sort({ [sortBy]: order }).skip(skip).limit(limit);
+  if (!data) {
+    throw new ApiError(404, "No courses found")
+  }
+  const totalCount = await Course.countDocuments();
+  return res.status(200)
+    .json(new ApiResponse(200, { data, currentPage: page, totalCount: Math.ceil(totalCount / limit) }, "successfully fetch data"));
+})
+
+const getSingleCourse = asyncHandler(async (req, res) => {
+  const id = req.params.id;
+  if (!id) {
+    throw new ApiError(401, "Please provide course id")
+  }
+  const singleData = await Course.findById(id);
+  if (!singleData) {
+    throw new ApiError(404, "course not found")
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, singleData, "successfully fetch data"))
+})
+
+const addSyllabus = asyncHandler(async (req, res) => {
+  const id = req.params.id;
+  if (!id) {
+    throw new ApiError(401, "Please provide course id")
+  }
+
+  const Data = await Course.findById(id)
+  if (!Data) {
+    throw new ApiError(404, "Course not found")
+  }
+
+  // upload on gallery
+  const localPath = req.file?.path;
+  if (!localPath) {
+    throw new ApiError(400, "Please provide syllabus")
+  }
+
+  const uploadSyllabus = await uploadOnCloudinary(localPath);
+  if (!uploadSyllabus) {
+    throw new ApiError(401, "upload file not recievd")
+  }
+
+  Data.syllabus.push(uploadSyllabus?.url);
+  const savefile = await Data.save({ validateBeforeSave: true });
+  if (!savefile) {
+    throw new ApiError(500, "something went wrong adding a syllabus file")
+  }
+  return res.status(200)
+    .json(new ApiResponse(200, "Successfully add Syllabus"))
+})
+
 export {
-  registerCourse
+  registerCourse,
+  getCourse,
+  getSingleCourse,
+  addSyllabus,
 }
--- server/src/utils/cloudinary.js
+++ server/src/utils/cloudinary.js
@@ -1,13 +1,17 @@
 import { v2 as cloudinary } from 'cloudinary';
 import fs from "fs";
-const uploadOnCloudinary = async function (localFilePath) {
-  // Configuration
-  cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_SECRET_KEY,
-  })
+import dotenv from "dotenv"
+import { raw } from 'express';
+dotenv.config({});
 
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET_KEY,
+})
+
+const uploadOnCloudinary = async function (localFilePath) {
   try {
     if (!localFilePath) {
       return console.log("File is not Found!", null, localFilePath);
@@ -26,7 +30,23 @@ const uploadOnCloudinary = async function (localFilePath) {
     fs.unlinkSync(localFilePath)
     return null
   }
-
 }
 
-export { uploadOnCloudinary };
+const deleteMediaFromCloudinary = async function (publicId) {
+  try {
+    await cloudinary.uploader.destroy(publicId)
+  } catch (error) {
+    console.log("Failed to delete on Cloudinary")
+    return
+  }
+}
+const deleteFileFromCloudinary = async function (publicId) {
+  try {
+    await cloudinary.uploader.destroy(publicId, { resource_type: raw })
+  } catch (error) {
+    console.log("Failed to delete on Cloudinary")
+    return error
+  }
+}
+
+export { uploadOnCloudinary, deleteMediaFromCloudinary, deleteFileFromCloudinary };
