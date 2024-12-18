474e8738083a4c410e648d81e4205841c10838c2
observingmyself <ps5380028@gmail.com>
2024-12-18 12:31:40 +0530

    redux-persist

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,10 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from './auth-slice/index'
+import storage from 'redux-persist/lib/storage'
+import {persistReducer} from 'redux-persist'
+import {combineReducers} from '@reduxjs/toolkit'
+
+const persistConfig = {
+    key : 'root',
+    version : 1,
+    storage,
+}
+
+const reducer = combineReducers({
+  auth: authReducer,
+});
+
+const persistReducers = persistReducer(persistConfig,reducer)
 
 const store = configureStore({
-    reducer : {
-        auth : authReducer
-    }
+    reducer : persistReducers
 })
 
 export default store
