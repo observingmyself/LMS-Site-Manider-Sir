39ea49d468e14b86dcf90e4421f4e7b367c28361
Moksh <moksh8516@gmail.com>
2024-12-20 12:15:54 +0530

    merge store

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,29 +1,27 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from './auth-slice/index'
 import storage from 'redux-persist/lib/storage'
-import {persistReducer} from 'redux-persist'
-import {combineReducers} from '@reduxjs/toolkit'
+import { persistReducer } from 'redux-persist'
+import { combineReducers } from '@reduxjs/toolkit'
 
 const persistConfig = {
-    key : 'root',
-    version : 1,
+    key: 'root',
+    version: 1,
     storage,
 }
 
 const reducer = combineReducers({
-  auth: authReducer,
+    auth: authReducer,
 });
 
-const persistReducers = persistReducer(persistConfig,reducer)
+const persistReducers = persistReducer(persistConfig, reducer)
 
 const store = configureStore({
-<<<<<<< HEAD
     reducer: {
         auth: authReducer
-    }
-=======
-    reducer : persistReducers
->>>>>>> 159930332e979af62d23056ea02bd8ae0a125de0
+    },
+    reducer: persistReducers
+
 })
 
 export default store
