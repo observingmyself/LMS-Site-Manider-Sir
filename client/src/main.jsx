197b5a0709cfa61bf759a0773a349b7939d3fcaf
Moksh <moksh8516@gmail.com>
2024-12-25 12:20:39 +0530

    comment PersistStore

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,9 +6,9 @@ import App from "./App.jsx";
 import { Provider } from "react-redux";
 import store from "./store/store.js";
 import { PersistGate } from "redux-persist/integration/react";
-import { persistStore } from "redux-persist";
+// import { persistStore } from "redux-persist";
 
-const persistor = persistStore(store);
+// const persistor = persistStore(store);
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
