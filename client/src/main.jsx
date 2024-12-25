352981178a97eab1f6af1515d8f2728fadc1d6f3
observingmyself <ps5380028@gmail.com>
2024-12-25 12:43:10 +0530

    course

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,6 +8,7 @@ import store from "./store/store.js";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
 
+// const persistor = persistStore(store);
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
