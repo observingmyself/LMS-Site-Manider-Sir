4acfb7b9d623cd79e5a1d855d61e7f3c55dcd02e
Moksh <moksh8516@gmail.com>
2025-01-24 15:22:00 +0530

    resolve build chunk error

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "client/node_modules"),
+      "@": path.resolve(__dirname, "client/src"),
+    },
+  },
   server: {
     proxy: {
       //'/api': 'http://localhost:3000'
     }
-  }
+  },
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
 })
