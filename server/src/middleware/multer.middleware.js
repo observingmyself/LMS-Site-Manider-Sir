164d08c6f768903116a0f2d81a2318797e052c17
Moksh <moksh8516@gmail.com>
2025-01-25 23:03:01 +0530

    update multer

--- server/src/middleware/multer.middleware.js
+++ server/src/middleware/multer.middleware.js
@@ -1,35 +1,15 @@
-// import multer from "multer";
-// import path from "path"
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, path.join(__dirname, 'public', 'temp'))
-//   },
-//   filename: function (req, file, cb) {
-//     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-//     let spliting = file.originalname.split(".", 2)
-//     let fileName = spliting[0] + "-" + uniqueSuffix + "." + spliting[1];
-//     cb(null, fileName)
-//   }
-// })
-
-// export const upload = multer({ storage: storage })
 import multer from "multer";
-import path from "path";
-
-// Set up storage configuration for Multer
+import path from "path"
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    // Ensure the destination path is correct
-    cb(null, path.join(__dirname, 'public', 'temp'));
+    cb(null, path.join(__dirname, 'public', 'temp'))
   },
   filename: function (req, file, cb) {
-    // Create a unique filename
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    let spliting = file.originalname.split(".", 2);
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    let spliting = file.originalname.split(".", 2)
     let fileName = spliting[0] + "-" + uniqueSuffix + "." + spliting[1];
-    cb(null, fileName);
+    cb(null, fileName)
   }
-});
+})
 
-// Export the upload middleware
-export const upload = multer({ storage: storage });
+export const upload = multer({ storage: storage })
