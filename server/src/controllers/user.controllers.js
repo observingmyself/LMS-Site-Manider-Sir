db3d327f8936097044fc7f3a4a04f67fb8dd670b
Moksh <moksh8516@gmail.com>
2025-01-25 11:03:31 +0530

    edit cookie

--- server/src/controllers/user.controllers.js
+++ server/src/controllers/user.controllers.js
@@ -24,7 +24,7 @@ const generateToken = async (userId) => {
 const options = {
   httpOnly: true,
   secure: true,
-  sameSite: None,
+  sameSite: "None",
 };
 
 const Register = asyncHandler(async (req, res) => {
