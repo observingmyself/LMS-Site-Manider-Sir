29493d7856155b56d89fc4c2ab04b76b04fd3b36
Moksh <moksh8516@gmail.com>
2025-01-27 03:03:25 +0530

    fix production url issue

--- server/src/connection/Db.js
+++ server/src/connection/Db.js
@@ -3,7 +3,7 @@ import { DB_Name } from "../constant.js"
 
 const connection = async () => {
   try {
-    const connect = await mongose.connect(`${process.env.MONGODB_URL}/${DB_Name}`)
+    const connect = await mongose.connect(`${process.env.MONGODB_URL}/${DB_Name}?authSource=admin`)
     console.log("MongoDb connection has been establish successfully On DB Host! " + connect.connection.host);
   } catch (error) {
     console.log("MongoDb is Failed to setup Connection " + error);
